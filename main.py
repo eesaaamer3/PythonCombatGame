793fe9a42fd3341b9e2695c1ebb54cffa3b738e0
Eesa <eesaaamer5@gmail.com>
2020-03-26 00:52:36 -0400

    Intro to Physics
    Added movement for both characters; implemented jumping for both

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import characters
 pygame.init()
 
 # Creates game window
-gameScreen = pygame.display.set_mode((1200, 800))
+gameScreen = pygame.display.set_mode((700, 400))
 
 # Title and Logo
 pygame.display.set_caption("Hero Combat")
@@ -13,19 +13,27 @@ icon = pygame.image.load('Images/superhero.png')
 pygame.display.set_icon(icon)
 
 
+# Background
+background = pygame.image.load('Images/background.png')
+
 # Player
 ironMan = pygame.image.load('Images/ironman.png')
-playerX = 370
-playerY = 480
+playerX = 20
+playerY = 275
 playerX_change = 0
-playerY_change = 0
+player_velocity = 0
+playerStanding = True
+playerJumping = False
 
 # Enemy 
 batman = pygame.image.load('Images/batman.png')
-enemyX = random.randint(0, 1200)
+enemyX = 550
 enemyY = 240
 enemyX_change = 0
-enemyY_change = 0
+enemy_velocity = 0
+enemyStanding = True
+enemyJumping = False
+
 
 # Changing image size
 finalPlayer = pygame.transform.scale(ironMan, (100, 100))
@@ -38,13 +46,14 @@ def player(x, y):
 def enemy(x, y):
     gameScreen.blit(finalEnemy, (x, y))
 
-def gravity()
+
 
 # Game Loop
 gameRun = True
 while gameRun:
     gameScreen.fill((255, 255, 255))
 
+    gameScreen.blit(background, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameRun = False
@@ -60,17 +69,69 @@ while gameRun:
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             playerX_change = 0
-    
-    playerX += playerX_change
 
-    # Boundries
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_a:
+            enemyX_change = -4
+        if event.key == pygame.K_d:
+            enemyX_change = 4
+        
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_a or event.key == pygame.K_d:
+            enemyX_change = 0
+
+
+    # Iron Man Jumping
+    if playerStanding == True:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player_velocity = -12
+                playerJumping = True
+                playerStanding = False
+    elif playerJumping == True:
+        player_velocity += 0.5
+        if playerY > 275:
+            player_velocity = 0
+            playerY = 275
+            playerJumping = False
+            playerStanding = True
+
+    # Batman Jumping
+    if enemyStanding == True:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                enemy_velocity = -12
+                enemyJumping = True
+                enemyStanding = False
+    elif enemyJumping == True:
+        enemy_velocity += 0.5
+        if enemyY > 240:
+            enemy_velocity = 0
+            enemyY = 240
+            enemyJumping = False
+            enemyStanding = True
+
+    playerY += player_velocity
+    enemyY += enemy_velocity
+
+    playerX += playerX_change
+    enemyX += enemyX_change
+
+
+    # Boundries on the side walls
     if playerX < 0:
         playerX = 0
-    elif playerX >= 1100:
-        playerX = 1100  
+    elif playerX >= 600:
+        playerX = 600 
+    
+    if enemyX < 0:
+        enemyX = 0
+    elif enemyX >= 600:
+        enemyX = 600
 
+    
     player(playerX, playerY)
     enemy(enemyX, enemyY)
-    
+
     pygame.display.update()
 
